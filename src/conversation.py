83a36aa693a372fe12742d91cab25eedb5d075d0
tono || トノ <64560893+smtono@users.noreply.github.com>
2022-09-16 22:54:08 -0500

    cleanup

--- src/conversation.py
+++ src/conversation.py
@@ -4,31 +4,45 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-gordon = 'gordon_personality.txt'
-jackass = 'jackass_personality.txt'
-nerd = 'nerd_personality.txt'
+sumire = os.path.join(os.getcwd(), 'src', 'scripts', 'sumire.txt')
+gordon = os.path.join(os.getcwd(), 'src', 'scripts', 'gordon_personality.txt')
+jackass = os.path.join(os.getcwd(), 'src', 'scripts', 'jackass_personality.txt')
+nerd = os.path.join(os.getcwd(), 'src', 'scripts', 'nerd_personality.txt')
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 start_sequence = "\nSumire:"
 input_prompt = "\n\nUser:"
 session_prompt = ""
-with open(gordon,'r') as file:
+
+with open(sumire, 'r') as file:
     session_prompt = file.read()
 chat_log = f'{session_prompt}'
 
-def ask(question): #question, chat_log=None
+def ask(question: str) -> str:
+    """
+    Generates a response to a question using the GPT-3 API
+
+    Args:
+        question: str
+            The question raised by the user
+    Returns:
+        A string response to the question
+    """
+
     prompt_text = f'{chat_log}{input_prompt}: {question}{start_sequence}:'
+    
     response = openai.Completion.create(
       engine="davinci",
       prompt=prompt_text,
-      temperature=0.5,
       max_tokens=250,
-      top_p=1,
+      temperature=0.9,
+      n=1,
       frequency_penalty=0,
       presence_penalty=0.3,
       stop=["\n"],
     )
+
     story = response['choices'][0]['text']
     chat_log.join("\n"f'{input_prompt} {question}{start_sequence}{story}')
     return str(story)
