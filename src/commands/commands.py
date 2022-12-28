2fdca79c7faec5a1f26bc736cb3950f41d2f33ec
tono || トノ <64560893+smtono@users.noreply.github.com>
2022-12-27 23:47:44 -0600

    commands skeleton

---
+++ src/commands/commands.py
@@ -0,0 +1,53 @@
+"""
+Contains commands for interacting with Sumire
+
+Commands include:
+    - help
+    - ping
+    - info
+    - invite
+    - talk
+    - dalle
+"""
+
+import discord
+
+def parse_command(command):
+    """
+    Parses a command and returns a response
+    """
+    
+
+def help_command():
+    """
+    Returns a help embed
+    """
+    embed = discord.Embed(
+        title="Help",
+        description="Sumire is a bot that can talk to you and generate images. \n\n"
+    )
+
+def ping_command():
+    """
+    Returns ping
+    """
+    
+def info_command():
+    """
+    Returns an info embed about a command
+    """
+
+def invite_command():
+    """
+    Returns an invite link
+    """
+
+def talk_command():
+    """
+    Returns a response to a user's input
+    """
+
+def dalle_command():
+    """
+    Returns a generated image
+    """
