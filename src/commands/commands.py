a709a9f7c19709144389bd8cff888a3ba885f9e0
tono || トノ <64560893+smtono@users.noreply.github.com>
2022-12-28 20:25:49 -0600

    add parser

--- src/commands/commands.py
+++ src/commands/commands.py
@@ -12,42 +12,66 @@ Commands include:
 
 import discord
 
-def parse_command(command):
-    """
-    Parses a command and returns a response
-    """
-    
+class Parser():
+    commands = {
+        "help": self.help_command,
+        "ping": self.ping_command,
+    }
 
-def help_command():
-    """
-    Returns a help embed
-    """
-    embed = discord.Embed(
-        title="Help",
-        description="Sumire is a bot that can talk to you and generate images. \n\n"
-    )
+    def parse_command(self, args: list):
+        """
+        Parses a command and returns a response
+        
+        Args:
+            args: list
+                List of arguments passed by the user
+        Returns:
+            None
+        """
+        command = args[0]   # command to execute
+        args = args[1:]     # args to pass to command
+        
+        # Get the command function
+        command_function = self.commands.get(command)
+        
+        # If the command is not found, print an error message
+        if command_function is None:
+            print(f'Command {command} not found')
+            return
+        else:
+            getattr(self, command)(args)
+        
 
-def ping_command():
-    """
-    Returns ping
-    """
-    
-def info_command():
-    """
-    Returns an info embed about a command
-    """
+    def help_command(self):
+        """
+        Returns a help embed
+        """
+        embed = discord.Embed(
+            title="Help",
+            description="Sumire is a bot that can talk to you and generate images. \n\n"
+        )
 
-def invite_command():
-    """
-    Returns an invite link
-    """
+    def ping_command(self):
+        """
+        Returns ping
+        """
+        
+    def info_command(self):
+        """
+        Returns an info embed about a command
+        """
 
-def talk_command():
-    """
-    Returns a response to a user's input
-    """
+    def invite_command(self):
+        """
+        Returns an invite link
+        """
 
-def dalle_command():
-    """
-    Returns a generated image
-    """
+    def talk_command(self):
+        """
+        Returns a response to a user's input
+        """
+
+    def dalle_command(self):
+        """
+        Returns a generated image
+        """
